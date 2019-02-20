a4f7a73daed5b7a99d2c838d73d396aa153a4068
Alex Rauenzahn <xandermv@yahoo.com>
2019-02-20 16:07:01 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/life/controller/Life.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.life.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class Life {
+
+
+  @FXML
+  private Button start;
+
+
+
+  @FXML
+  private ScrollPane viewScroller;
+}
